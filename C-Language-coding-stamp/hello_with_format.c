79dc74738e502beb0d7dedf2435bc4529cf467b1
dhampir7 <dhampir7@naver.com>
2023-02-06 22:18:38 +0900

    초기 커밋.

---
+++ C-Language-coding-stamp/hello_with_format.c
@@ -0,0 +1,16 @@
+//#include <stdio.h>
+//
+//int main()
+//{
+//    printf("%s, %s\n", "Hello", "1234");
+//
+//    printf("%s, %s\n", "Hello,", "1234");
+//
+//    printf("%s%s\n", "Hello", "1234");
+//
+//    printf("%s%s\n", "Hello ", "1234");
+//
+//    printf("%s%s\n", "Hello", " 1234");
+//
+//    return 0;
+//}
---
+++ C-Language-coding-stamp/practice_hello.c
@@ -0,0 +1,8 @@
+//#include <stdio.h>
+//
+//int main() {
+//	printf("Hello, world!\n");
+//	printf("1234567890\n");
+//
+//	return 0;
+//}
