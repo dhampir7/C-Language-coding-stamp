1f7a9c0a4ba63aaa780f9b391dfe10c92c2fe9ed
dhampir7 <dhampir7@naver.com>
2023-03-20 19:44:51 +0900

    3/20

--- C-Language-coding-stamp/Unit36/judge_smallest_number.c
+++ C-Language-coding-stamp/Unit36/judge_smallest_number.c
@@ -1,18 +1,18 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-
-int main()
-{
-    int numArr[5];
-    int smallestNumber;
-
-    scanf("%d %d %d %d %d", &numArr[0], &numArr[1], &numArr[2], &numArr[3], &numArr[4]);
-
-    for (int i = 0; i < 5; i++) {
-        if (numArr[i] < smallestNumber) smallestNumber = numArr[i];
-    }
-
-    printf("%d\n", smallestNumber);
-
-    return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int numArr[5];
+//    int smallestNumber;
+//
+//    scanf("%d %d %d %d %d", &numArr[0], &numArr[1], &numArr[2], &numArr[3], &numArr[4]);
+//
+//    for (int i = 0; i < 5; i++) {
+//        if (numArr[i] < smallestNumber) smallestNumber = numArr[i];
+//    }
+//
+//    printf("%d\n", smallestNumber);
+//
+//    return 0;
+//}
